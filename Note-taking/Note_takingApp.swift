60cb67737cf9af22c5230d29c2ba9fde8a77f815
Yazeedj <glzyww22@gmail.com>
2026-03-26 14:26:23 -0400

    Initial Commit

---
+++ Note-taking/Note_takingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_takingApp.swift
+//  Note-taking
+//
+//  Created by Yazeed Jameel on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct Note_takingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
